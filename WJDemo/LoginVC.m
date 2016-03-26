28982223cb561db40e972522a0e15596c1e6e857
nodreamnolove <scxueba3@duanruiquandemac-mini-2.local>
2016-03-26 08:31:18 +0800

    登录成功

--- WJDemo/LoginVC.m
+++ WJDemo/LoginVC.m
@@ -45,19 +45,36 @@
 //    mgr.responseSerializer = [AFJSONResponseSerializer serializer];
     mgr.requestSerializer = [AFJSONRequestSerializer serializer];
     mgr.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/json", @"text/plain", @"text/html", @"text/javascript", nil];
-    NSString *urlstring = @"http://wanjiwuhanyuming.oicp.net:8082/OBU/";
-    NSMutableDictionary *params = [NSMutableDictionary dictionary];
-    [params setObject:self.userNameF.text forKey:@"CustomerAccount"];
-    [params setObject:self.passwordF.text forKey:@"Password"];
-    [params setObject:@"CustomerLogin" forKey:@"CmdType"];
-    [params setObject:@"android" forKey:@"LoginType"];
+    NSString *urlstring = @"http://wanjiwuhanyuming.oicp.net:8082/OBU/WJOBU";
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];//@{@"CmdType":@"CustomerLogin",@"CustomerAccount":self.userNameF.text,@"Password":self.passwordF.text,@"LoginType":@"android"};
+//    [params setObject:self.userNameF.text forKey:@"CustomerAccount"];
+//    [params setObject:self.passwordF.text forKey:@"Password"];
+//    [params setObject:@"CustomerLogin" forKey:@"CmdType"];
+//    [params setObject:@"android" forKey:@"LoginType"];
     
-    [mgr POST:urlstring parameters:params success:^(NSURLSessionDataTask *task, id responseObject) {
+    [mgr POST:ServerUrl parameters:params success:^(NSURLSessionDataTask *task, id responseObject) {
         NSLog(@"responseObject = %@",responseObject);
     } failure:^(NSURLSessionDataTask *task, NSError *error) {
         NSLog(@"%@",error);
     }];
     
+    
+    
+//    NSString *usrstring = @"http://wanjiwuhanyuming.oicp.net:8082/OBU/WJOBU";
+//    
+//    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+//    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+//    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/json", @"text/plain", @"text/html", @"text/javascript", nil];
+//    
+//    NSDictionary *param = @{@"CmdType":@"CustomerLogin",@"CustomerAccount":@"shaochong",@"Password":@"123456",@"LoginType":@"android"};
+//    
+//    
+//    [manager POST:usrstring parameters:param success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//        NSLog(@"%@",responseObject);
+//        
+//    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//        NSLog(@"%@",error);
+//    }];
     
 }
 @end
